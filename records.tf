b333d115ba8f1a52680605d57937d2579b3ce8fc
Coty Hamilton <13724650+cotyhamilton@users.noreply.github.com>
2024-03-31 14:20:39 -0400

    Records to redirect main site to github

--- records.tf
+++ records.tf
@@ -26,11 +26,43 @@ resource "digitalocean_record" "cotyhamilton_ns3" {
   ttl    = 1800
 }
 
-resource "digitalocean_record" "cotyhamilton_root" {
+resource "digitalocean_record" "cotyhamilton_root1" {
   domain = digitalocean_domain.cotyhamilton.id
   type   = "A"
   name   = "@"
-  value  = "76.76.21.21"
+  value  = "185.199.108.153"
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "cotyhamilton_root2" {
+  domain = digitalocean_domain.cotyhamilton.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.109.153"
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "cotyhamilton_root3" {
+  domain = digitalocean_domain.cotyhamilton.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.110.153"
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "cotyhamilton_root4" {
+  domain = digitalocean_domain.cotyhamilton.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.111.153"
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "cotyhamilton_www" {
+  domain = digitalocean_domain.cotyhamilton.id
+  type   = "CNAME"
+  name   = "www"
+  value  = "cotyhamilton.github.io."
   ttl    = 3600
 }
 
@@ -40,14 +72,6 @@ resource "digitalocean_record" "cotyhamilton_wildcard" {
   name   = "*"
   value  = digitalocean_droplet.cotyhamilton.ipv4_address
   ttl    = 60
-}
-
-resource "digitalocean_record" "cotyhamilton_www" {
-  domain = digitalocean_domain.cotyhamilton.id
-  type   = "CNAME"
-  name   = "www"
-  value  = "cname.vercel-dns.com."
-  ttl    = 3600
 }
 
 resource "digitalocean_record" "cotyhamilton_fm" {
